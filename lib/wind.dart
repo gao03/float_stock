4d387c6c8570b36e815a79e9a81c6da2976c07b1
gaozhiqiang03 <gaozhiqiang03@meituan.com>
2024-11-09 12:16:46 +0800

    ref4

--- lib/wind.dart
+++ lib/wind.dart
@@ -1,11 +1,9 @@
 import 'dart:async';
 import 'dart:convert';
-import 'dart:developer';
-
-import 'package:float_stock/entity.dart';
-import 'package:float_stock/utils.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/scheduler.dart';
+import 'package:float_stock/entity.dart';
+import 'package:float_stock/utils.dart';
 import 'package:flutter_floatwing/flutter_floatwing.dart';
 import 'package:longport/entity.dart';
 import 'package:longport/longport.dart';
@@ -22,155 +20,135 @@ class FloatWindowView extends StatefulWidget {
 class _FloatWindowViewState extends State<FloatWindowView> {
   late AppConfig config;
   List<StockInfo>? stockList;
-
-  Window? w;
+  Window? window;
   final int stockHeightBase = 400;
-  final Longport ls = Longport();
-  bool lsInit = false;
+  final Longport longportService = Longport();
+  bool isLongportInitialized = false;
 
   @override
   void initState() {
-    WidgetsFlutterBinding.ensureInitialized();
-
     super.initState();
     config = widget.config;
-    checkAndInitLs();
-    updateStockList(config.stockList);
+    _initializeLongport();
+    _updateStockList(config.stockList);
 
     SchedulerBinding.instance.addPostFrameCallback((_) {
-      w = Window.of(context);
-      w?.onData((source, name, data) async {
+      window = Window.of(context);
+      window?.onData((source, name, data) async {
         if (name == "config") {
-          refresh(AppConfig.fromJson(jsonDecode(data)));
+          _refresh(AppConfig.fromJson(jsonDecode(data)));
         }
       });
     });
   }
 
-  void checkAndInitLs() async {
-    var c = config.longPortConfig;
-    if (c != null && !lsInit) {
-      ls.init(c.appKey, c.appSecret, c.accessToken, onQuote);
-      lsInit = true;
+  void _initializeLongport() async {
+    var longportConfig = config.longPortConfig;
+    if (longportConfig != null && !isLongportInitialized) {
+      longportService.init(longportConfig.appKey, longportConfig.appSecret, longportConfig.accessToken, _onQuoteReceived);
+      isLongportInitialized = true;
     }
   }
 
-  Future<void> onQuote(String symbol, PushQuote quote) async {
+  Future<void> _onQuoteReceived(String symbol, PushQuote quote) async {
+    if (stockList == null) return;
+
     for (var stock in stockList!) {
-      debugPrint("onQuote: $symbol");
-      if (symbol.startsWith(convertStockCode(stock).toUpperCase()) &&
-          quote.lastDone != null) {
-        stock.color = getStockColor(quote.lastDone, stock.lastPrice);
+      if (symbol.startsWith(_convertStockCode(stock).toUpperCase()) && quote.lastDone != null) {
+        stock.color = _getStockColor(quote.lastDone, stock.lastPrice);
         stock.lastPrice = quote.lastDone!;
         break;
       }
     }
     setState(() {
-      stockList = stockList?.where(checkStockCanShow).toList();
+      stockList = stockList?.where(_canStockBeShown).toList();
     });
   }
 
-  bool checkStockCanShow(StockInfo stock) {
-    if (!stock.showInFloat) {
-      return false;
-    }
-    return checkMarketStatus(stock.type) != MarketStatus.close;
+  bool _canStockBeShown(StockInfo stock) {
+    return stock.showInFloat && checkMarketStatus(stock.type) != MarketStatus.close;
   }
 
-  void refresh(AppConfig newConfig) async {
+  void _refresh(AppConfig newConfig) async {
     setState(() {
       config = newConfig;
-      updateStockList(config.stockList);
+      _updateStockList(config.stockList);
     });
-    checkAndInitLs();
-    await w?.show(visible: newConfig.floatConfig.enable);
-    // var screenHeight = w?.system?.screenHeight ?? newConfig.floatConfig.screenHeight;
-    var screenWidth =
-        w?.system?.screenWidth ?? newConfig.floatConfig.screenWidth;
+    _initializeLongport();
+    await window?.show(visible: newConfig.floatConfig.enable);
+
+    var screenWidth = window?.system?.screenWidth ?? newConfig.floatConfig.screenWidth;
     if (screenWidth == 0) {
       screenWidth = newConfig.floatConfig.screenWidth;
     }
-    await w?.update(WindowConfig(
-      height: (newConfig.floatConfig.windowHeight *
-              stockHeightBase *
-              stockList!.length)
-          .toInt(),
+    await window?.update(WindowConfig(
+      height: (newConfig.floatConfig.windowHeight * stockHeightBase * stockList!.length).toInt(),
       width: (newConfig.floatConfig.windowWidth * screenWidth).toInt(),
     ));
   }
 
-  void updateStockList(List<StockInfo> newStockList) {
-    debugPrint("updateStockList all: ${jsonEncode(newStockList)}");
-    var filterStockList = newStockList
-        .where((i) => i.showInFloat == true)
-        .where(checkStockCanShow)
-        .toList();
-    debugPrint("updateStockList filter: ${jsonEncode(filterStockList)}");
-    ls.subscribes(filterStockList
-        .map((e) => convertStockCode(e))
-        .toList()
-        .cast<String>());
+  void _updateStockList(List<StockInfo> newStockList) {
+    var filteredStockList = newStockList
+            .where((stock) => stock.showInFloat)
+            .where((stock) => checkMarketStatus(stock.type) != MarketStatus.close)
+            .toList();
+    longportService.subscribes(filteredStockList.map(_convertStockCode).toList());
     setState(() {
-      stockList = filterStockList;
+      stockList = filteredStockList;
     });
   }
 
-  String convertStockCode(StockInfo stock) {
+  String _convertStockCode(StockInfo stock) {
     var code = stock.symbol;
     if (stock.type == "HK") {
-      while (code.startsWith("0")) {
-        code = code.substring(1);
-      }
+      code = code.replaceFirst(RegExp(r'^0+'), '');
     }
     return code;
   }
 
-  String generateStockText(StockInfo stock) {
+  String _generateStockText(StockInfo stock) {
     return formatNum(stock.lastPrice);
   }
 
-  Color getStockColor(double? curPrice, double? oldPrice) {
-    return curPrice == oldPrice || curPrice == null || oldPrice == null
-        ? Colors.black
-        : curPrice > oldPrice
-            ? Colors.red
-            : Colors.green;
+  Color _getStockColor(double? currentPrice, double? previousPrice) {
+    if (currentPrice == null || previousPrice == null || currentPrice == previousPrice) {
+      return Colors.black;
+    }
+    return currentPrice > previousPrice ? Colors.red : Colors.green;
   }
 
   @override
   Widget build(BuildContext context) {
     return SizedBox(
-        width: config.floatConfig.windowWidth * config.floatConfig.screenWidth,
-        height: config.floatConfig.windowHeight *
-            stockHeightBase *
-            stockList!.length,
-        child: GestureDetector(
-            onDoubleTap: () {
-              w?.launchMainActivity();
-            },
-            onLongPress: () {
-              // w?.hide();
-            },
-            child: Card(
-                elevation: 0,
-                color: Colors.white.withOpacity(config.floatConfig.opacity),
-                child: Column(children: [
-                  const Padding(
-                    padding: EdgeInsets.all(3),
-                  ),
-                  for (var stock in stockList!)
-                    Column(children: [
-                      Text(generateStockText(stock),
-                          style: TextStyle(
-                            color: stock.color,
-                            fontSize: config.floatConfig.fontSize * 100,
-                          ),
-                          overflow: TextOverflow.ellipsis,
-                          textAlign: TextAlign.left),
-                      Offstage(
-                          offstage: (stock.symbol == stockList!.last.symbol),
-                          child: const Divider(indent: 15)),
-                    ])
-                ]))));
+      width: config.floatConfig.windowWidth * config.floatConfig.screenWidth,
+      height: config.floatConfig.windowHeight * stockHeightBase * stockList!.length,
+      child: GestureDetector(
+        onDoubleTap: window?.launchMainActivity,
+        child: Card(
+          elevation: 0,
+          color: Colors.white.withOpacity(config.floatConfig.opacity),
+          child: Column(
+            children: [
+              const Padding(padding: EdgeInsets.all(3)),
+              for (var stock in stockList!)
+                Column(
+                  children: [
+                    Text(
+                      _generateStockText(stock),
+                      style: TextStyle(
+                        color: stock.color,
+                        fontSize: config.floatConfig.fontSize * 100,
+                      ),
+                      overflow: TextOverflow.ellipsis,
+                      textAlign: TextAlign.left,
+                    ),
+                    if (stock.symbol != stockList!.last.symbol) const Divider(indent: 15),
+                  ],
+                ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
